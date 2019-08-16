061f64675b31955c8d5c6ade3f3cff65df0df6db
Calvin Baart <calvin.baart@gmail.com>
2019-08-16 14:59:03 +0200

    Actually use phpunit instead of a random test case...
    Signed-off-by: Calvin Baart <calvin.baart@gmail.com>

--- Laravel.Tests/main.php
+++ Laravel.Tests/main.php
@@ -9,31 +9,22 @@
 * file that was distributed with this source code.
 */
 
-// require_once(__DIR__ . "/vendor/laravel_autoload.php");
-// require_once(__DIR__ . "/vendor/autoload.php");
+require_once(__DIR__ . "/vendor/laravel_autoload.php");
+require_once(__DIR__ . "/vendor/autoload.php");
 
-// function main()
-// {
-//     if (!ini_get('date.timezone')) {
-//         ini_set('date.timezone', 'UTC');
-//     }
-
-//     $options = getopt('', array('prepend:'));
-//     if (isset($options['prepend'])) {
-//         require $options['prepend'];
-//     }
-//     unset($options);
-
-//     PHPUnit\TextUI\Command::main();
-// }
-
-// main();
-
-class TestIteratorAggregate implements \IteratorAggregate
+function main()
 {
-    public function getIterator() {
-        return new \ArrayIterator($this);
+    if (!ini_get('date.timezone')) {
+        ini_set('date.timezone', 'UTC');
     }
+
+    $options = getopt('', array('prepend:'));
+    if (isset($options['prepend'])) {
+        require $options['prepend'];
+    }
+    unset($options);
+
+    PHPUnit\TextUI\Command::main();
 }
 
-$tmp = iterator_to_array(new TestIteratorAggregate());
+main();
